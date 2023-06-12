f5a2156b30e33ad6ee1cb839c102ddc628f50d0e
saket <saketthakur4797@gmail.com>
2023-06-12 23:00:55 +0530

    I am going to delete some stuff

--- audible scrape.py
+++ audible scrape.py
@@ -1,8 +1,19 @@
+import re
 import requests
 from bs4 import BeautifulSoup
+import sqlite3
+import json
+import datetime
+
+def get_authors(text, string=True):
+  try:
+    return text.split('by: ')[1].split(', ')
+  except:
+    if string: return text.join(', ')
+    return  [None]
 
 # Send a GET request to the URL of this page
-def generate_link(page=1, audible_programs="20956260011", author_author="", keywords="", narrator="full-cast", publisher="", sort="review-rank", title="", pageSize=100):
+def generate_link(page=1, audible_programs="20956260011", author_author="", keywords="", narrator="full-cast", publisher="", sort="review-rank", title="", pageSize=50):
   base_url = "https://www.audible.com/search?"
   params = {
     "audible_programs": audible_programs,
@@ -19,11 +30,66 @@ def generate_link(page=1, audible_programs="20956260011", author_author="", keyw
     "pageLoadId": "dELu6hUurPGV8fAu",
     "creativeId": "9648f6bf-4f29-4fb4-9489-33163c0bb63e"
   }
-
   if page > 1:
     params["page"] = page
   query = "&".join([f"{key}={value}" for key, value in params.items()])
   return base_url + query
+
+
+
+# convert string to date object
+def string_to_date(text):
+    '''
+    Convert string to date object
+    datetime.date
+        year in float
+        ex: 2013.2993150684931
+    '''
+    if text == None:
+        return None
+    elif 'Release date: ' in text:
+        month, day, year = text.split('Release date: ')[1].split('-')
+        year = "20"+year
+        # month, day, year = map(int, text.split('-'))
+        date =  datetime.date(int(year), int(month), int(day))
+    # check if text is float or int
+    elif text.isnumeric():
+        return text
+    return date.year+ date.month/12 + date.day/365
+
+# convert string to date object
+def extract_rating(string):
+    if string == "Not rated yet" or string == None:
+        return None, None
+    string = string.split(' out of 5 stars ')
+    rating = float(string[0])
+    votes = int(string[1].split(' rating')[0].replace(',',''))
+    return rating, votes
+
+
+# convert string to date object
+def extract_rating(string):
+    if string == "Not rated yet" or string == None:
+        return None, None
+    string = string.split(' out of 5 stars ')
+    rating = float(string[0])
+    votes = int(string[1].split(' rating')[0].replace(',',''))
+    return rating, votes
+
+
+# hour and min to min
+def hour_min_to_min(tim):
+    if tim == None:
+        return None
+    elif 'min' not in tim:
+        return int(tim.split('Length: ')[1].split(' hr')[0])*60
+    elif 'hr' not in tim:
+        return int(tim.split('Length: ')[1].split(' min')[0])
+    else:
+        hr = tim.split('Length: ')[1].split(' hr')[0]
+        minute = tim.split("and ")[1].split(' min')[0]
+    return int(hr)*60 + int(minute)
+
 
 # Define a function to scrape all details from a page
 def scrape_all_details(page):
@@ -56,7 +122,7 @@ def scrape_all_details(page):
     except:
       src = None
       urls.append(src)
-      # print(src) # Print the image URL
+  # print(src) # Print the image URL
   cover_image = []
   for image_link in urls:
     if "https://m.media-amazon.com/images/I" in image_link or ".jpg" in image_link:
@@ -81,7 +147,7 @@ def scrape_all_details(page):
     try:
       title = product.find("h3", class_="bc-heading").text.strip()
       details_dict["title"] = title
-    except AttributeError:
+    except :
       # Assign None if title is not found
       details_dict["title"] = None
 
@@ -89,55 +155,55 @@ def scrape_all_details(page):
     try:
       subtitle = product.find("li", class_="bc-list-item subtitle").text.strip()
       details_dict["subtitle"] = subtitle
-    except AttributeError:
+    except :
       # Assign None if subtitle is not found
       details_dict["subtitle"] = None
 
     # Try to find the author element and handle the exception if not found
     try:
       author = product.find("li", class_="authorLabel").text.strip()
-      details_dict["author"] = author
-    except AttributeError:
+      details_dict["author"] = author.split("By: ")[1]
+    except :
       # Assign None if author is not found
       details_dict["author"] = None
 
     # Try to find the narrator element and handle the exception if not found
     try:
       narrator = product.find("li", class_="narratorLabel").text.strip()
-      details_dict["narrator"] = narrator
-    except AttributeError:
+      details_dict["narrator"] = narrator.split("Narrated by: ")[1]
+    except :
       # Assign None if narrator is not found
       details_dict["narrator"] = None
 
     # Try to find the series element and handle the exception if not found
     try:
       series = product.find("li", class_="seriesLabel").text.strip()
-      details_dict["series"] = series
-    except AttributeError:
+      details_dict["series"] = series.split("Series: ")[1]
+    except :
       # Assign None if series is not found
       details_dict["series"] = None
 
     # Try to find the length element and handle the exception if not found
     try:
       length = product.find("li", class_="runtimeLabel").text.strip()
-      details_dict["length"] = length
-    except AttributeError:
+      details_dict["length"] = hour_min_to_min(length)
+    except :
       # Assign None if length is not found
       details_dict["length"] = None
 
     # Try to find the release date element and handle the exception if not found
     try:
       release_date = product.find("li", class_="releaseDateLabel").text.strip()
-      details_dict["release_date"] = release_date
-    except AttributeError:
+      details_dict["release_date"] = release_date.split("Release date: ")[1]
+    except :
       # Assign None if release date is not found
       details_dict["release_date"] = None
 
     # Try to find the language element and handle the exception if not found
     try:
       language = product.find("li", class_="languageLabel").text.strip()
-      details_dict["language"] = language
-    except AttributeError:
+      details_dict["language"] = language.split("Language: ")[1]
+    except :
       # Assign None if language is not found
       details_dict["language"] = None
 
@@ -145,7 +211,7 @@ def scrape_all_details(page):
     try:
       summary = product.find("p", class_="bc-text").text.strip()
       details_dict["summary"] = summary
-    except AttributeError:
+    except :
       # Assign None if summary is not found
       details_dict["summary"] = None
 
@@ -153,7 +219,7 @@ def scrape_all_details(page):
     try:
       image = product.find("img").get("src")
       details_dict["image"] = image
-    except AttributeError:
+    except :
       # Assign None if image is not found
       details_dict["image"] = None
 
@@ -161,7 +227,7 @@ def scrape_all_details(page):
     try:
       link = product.find("a", class_="bc-link bc-color-link").get("href")
       details_dict["link"] = link
-    except AttributeError:
+    except :
       # Assign None if link is not found
       details_dict["link"] = None
     
@@ -169,7 +235,7 @@ def scrape_all_details(page):
     try:
       ratings = product.find("li", class_="ratingsLabel").text.strip()
       details_dict["ratings"] = ratings
-    except AttributeError:
+    except :
       # Assign None if ratings is not found
       details_dict["ratings"] = None
 
@@ -182,7 +248,7 @@ def scrape_all_details(page):
         value = re.sub("\s+", " ", value)
         # Update the dictionary with the formatted value
         details_dict[key] = value
-      except AttributeError:
+      except :
         pass
 
     # Append the dictionary to the list
@@ -197,11 +263,8 @@ def scrape_all_details(page):
   # Return the list with all the details
   return details_list
 
-
-# data = scrape_all_details()
-
-
+# request = requests.get(generate_link())
+# soup = BeautifulSoup(request.text, "html.parser")
 
 data = scrape_all_details(generate_link())
-
-# <img id="" class="bc-pub-blockbc-image-inset-border js-only-element" src="https://m.media-amazon.com/images/I/51Xt2BYA5vL._SL500_.jpg" loading="lazy" alt="We're Alive: A Story of Survival, the Third Season Audiobook By Kc Wayland cover art" width="100%" srcset="https://m.media-amazon.com/images/I/51Xt2BYA5vL._SL250_.jpg 1x, https://m.media-amazon.com/images/I/51Xt2BYA5vL._SL500_.jpg2x">
+# data[4]
